4573378f0777b2d9b51eff0e01ef6e703f0c407c
Mohammad-Asraful <asrafmail01@gmail.com>
2023-04-10 22:22:25 +0600

    material UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React from 'react'
-import ReactHookForm2 from './Components/ReactHookForm2'
-import ReactHookForm from './Components/ReactHookForm'
+import MaterialUI from './Components/MaterialUI'
+import VariantButtonGroup from './Components/ButtonGroup'
+import { Checkbox } from '@mui/material'
+import FloatingButton from './Components/FloatingButton'
+import RadioButtonsGroup from './Components/RadioGroup'
+import BasicRating from './Components/RatingGroup'
+import SelectOtherProps from './Components/BasicSelect'
+import ContinuousSlider from './Components/VolumeSlider'
+import BasicSwitches from './Components/BasicSwitches'
+import CustomizedDividers from './Components/ToggleButton'
+import Spinner from './Components/Spinner'
+import SimpleAccordion from './Components/SnackBar'
+import ResponsiveAppBar from './Components/NavBar'
+import OutlinedCard from './Components/AppBar'
+import TemporaryDrawer from './Components/MUIDrawer'
+import BasicModal from './Components/Modal'
+import DataGridProDemo from './Components/DataGrid'
+
 
 const App = () => {
   return (
     <div>
-      <ReactHookForm />
+      <ResponsiveAppBar />
 
-      <ReactHookForm2 />
+      <MaterialUI />
+
+      <VariantButtonGroup />
+
+      <Checkbox />
+
+      <FloatingButton />
+
+      <RadioButtonsGroup />
+
+      <BasicRating />
+
+      <SelectOtherProps />
+
+      <ContinuousSlider />
+
+      <BasicSwitches />
+
+      <CustomizedDividers />
+
+      <Spinner />
+
+      <SimpleAccordion />
+
+      <OutlinedCard />
+
+      <TemporaryDrawer />
+
+      <BasicModal />
+
+      <DataGridProDemo />
     </div>
   )
 }
